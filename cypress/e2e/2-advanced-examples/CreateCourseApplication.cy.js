82622dd355dc1e010824fef39c6e8c14af3e07d8
Your Name <daniyal.yuzee@gmail.com>
2024-03-04 00:14:01 +0500

    Your commit message here

---
+++ cypress/e2e/2-advanced-examples/CreateCourseApplication.cy.js
@@ -0,0 +1,22 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+  
+  describe('Visit the site, click on the Login link, and click on the Sign in button', () => {
+      it('should visit the site, click on the Login link, and click on the Sign in button', () => {
+  
+        
+        
+        cy.visit('https://dev.yuzee.click');
+  
+      cy.contains('Sign in').click();
+  //comment add
+      cy.get('.row > :nth-child(1) > .form-control').clear()
+      cy.get('.row > :nth-child(1) > .form-control').type('fdc65165a75b29@crankymonkey.info')
+  
+      cy.get('.pass-fild > .form-control').clear()
+      cy.get('.pass-fild > .form-control').type('Admin12345!')
+  //asd
+      cy.get('form.ng-dirty > .text-center > .btn').click()
+      })
+    })
